9741d036646927cc70c535d554516be5974f6b9a
Gustavo Ferrreira <gustavohenriquesud033@gmail.com>
2026-09-04 00:15:18 -0300

    feat: creation of the Contract entity

---
+++ System/Entities/Contract.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Entities
+{
+    internal class Contract
+    {
+        private int Number;
+        private DateTime Date;
+        private double Value;
+
+        
+
+        public Contract(int number, DateTime date, double value)
+        {
+            Number = number;
+            Date = date;
+            Value = value;
+        }
+        public int number
+        {
+            get { return Number; }
+            set { number = value; }
+
+        }
+        public DateTime date
+        {
+            get { return Date; }
+            set { date = value; }
+
+        }
+        public double valuer
+        {
+            get { return Value; }
+            set { valuer = value; }
+
+        }
+    }
+}
