2deed7db28e54c998822a3e75e452a446e6a2a42
Gustavo Ferrreira <gustavohenriquesud033@gmail.com>
2026-09-04 01:27:32 -0300

    feat: Create Services

---
+++ System/Entities/Services/PayPalService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Entities.Services
+{
+    internal class PayPaalService
+    {
+        public double paymentFee(double amount)
+        {
+            return amount * 0.02;
+
+        }
+        public double interest (double amount, int month)
+        {
+            return (amount * 0.01) * month;
+
+        }
+
+    }
+}
---
+++ System/Entities/Services/ServiceContract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Entities.Services
+{
+    internal class PayPalService
+    {
+
+        public void processContract(Contract contract, int Month)
+        {
+            
+           
+            for(int i = 1; i <= Month; i++)
+            {
+
+                contract.date = contract.date.AddMonths(i);
+                Console.WriteLine(contract.date);
+                
+            }
+        }
+    }
+}
